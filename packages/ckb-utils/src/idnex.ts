45cc751ecae94969f1a1ec1446a51a9ea5584c57
Keith <keithwhisper@gmail.com>
2018-12-20 20:37:56 +0800

    feat(wallet): init wallet

---
+++ packages/ckb-utils/src/idnex.ts
@@ -0,0 +1,21 @@
+class Utils {
+  static hex2bin = () => {
+    // TODO:
+  }
+
+  static bin2hex = () => {
+    // TODO:
+  }
+
+  static bin2prefixHex = () => {
+    // TODO:
+  }
+
+  static extractPkBin = (pkBin: Uint8Array) => {
+    // TODO:
+  }
+
+  static jsonScriptToTypeHash = (script: string) => {
+    // TODO:
+  }
+}
---
+++ packages/ckb-wallet/README.md
@@ -0,0 +1,11 @@
+# `ckb-wallet`
+
+> TODO: description
+
+## Usage
+
+```
+const ckbWallet = require('ckb-wallet');
+
+// TODO: DEMONSTRATE API
+```
