e9592dd990f08b219422c6f2d8854a5c76a56134
Keith <keithwhisper@gmail.com>
2019-01-17 13:59:57 +0800

    docs(typedef): script model
    add more detailed description on script model

--- packages/ckb-types/index.d.ts
+++ packages/ckb-types/index.d.ts
@@ -1,6 +1,7 @@
-// https://github.com/nervosnetwork/ckb/blob/develop/rpc/src/types/blockchain.rs
+/**
+ * @see https://github.com/nervosnetwork/ckb/blob/develop/rpc/src/types/blockchain.rs for more infomation
+ */
 
-/* eslint-disable camelcase */
 declare namespace CkbComponents {
   export type LocalNodeId = string
   export type Hash = string
@@ -20,21 +21,24 @@ declare namespace CkbComponents {
    * RPC Units
    */
 
+  /* eslint-disable max-len */
   /**
    * @typedef IScript, lock or unlock script
-   * @property version, script version
-   * @property args, arguments
-   * @property reference, point to its dependency
-   * @property binary, its dependency
-   * @property signed_args, arguments
+   * @description IScript, the script model in CKB. CKB scripts use UNIX standard execution environment. Each script binary should contain a main function with the following signature `int main(int argc, char* argv[]);`. CKB will concat `signed_args` and `args`, then use the concatenated array to fill `argc/argv` part, then start the script execution. Upon termination, the executed `main` function here will provide a return code, `0` means the script execution succeeds, other values mean the execution fails.
+   * @property version, script version, used to resolve incompatible upgrade.
+   * @property args, normal arguments.
+   * @property reference, point to its dependency, if your script already exists on CKB, you can use this field to reference the script instead of including it again. You can just put the script hash in this reference field, then list the cell containing the script as an outpoint in deps field in the current transaction. CKB will automatically locate cell and load the binary from there and use it as script `binary` part. Notice this only works when you don't provide a `binary` field value, otherwise the value in the `binary` field always take precedence.
+   * @property binary, its dependency, ELF formatterd binary containing the actual RISC-V based script
+   * @property signed_args, signed arguments, used to enable script sharing: assume 2 CKB users both want to use secp256k1 algorithm to secure their cells, in order to do this, they will need scripts for secp256k1 verification, the scripts will also need to include their public key respectively. If they put public key directly in the script binary, the difference in public keys will lead to different script binaries, which is quite a waste of resource considering the majority part of 2 scripts here is exeactly the same. To solve this problem, they can each put their public key in `signed_args` part of the script model, then leverage the same secp256k1 script binary. By this way they can save as much resource as they can while preserving different ownerships. This might not be a huge save when we are talking 2 users, but as the number of users grow, the resource we can save with this scheme is huge.
+   * @tutorial Each script has a `type_hash` which uniquely identifies the script, for example, the `type_hash` of unlock script, is exactly the corresponding `lock` script field value in the referenced cell, when calculating type hash for a script, `version`, `bianry`, `reference` and `signed_args` will all be used.
    */
-
+  /* eslint-enable max-len */
   export interface IScript {
     version: number
     args: Uint8Array[]
     reference?: Hash
     binary?: Uint8Array
-    signed_args: Uint8Array[]
+    signedArgs: Uint8Array[]
   }
 
   /**
@@ -44,7 +48,7 @@ declare namespace CkbComponents {
    */
   export interface ICellInput {
     prevOutput: IOutPoint
-    unlock: IScritp
+    unlock: IScript
   }
 
   /**
@@ -169,5 +173,3 @@ declare namespace CkbComponents {
     outPoint?: IOutPoint
   }
 }
-
-/* eslint-enable camelcase */
