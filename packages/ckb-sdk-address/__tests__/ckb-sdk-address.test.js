33ed13e2478520c4b1963e89d6897037aa53d808
Keith <keithwhisper@gmail.com>
2019-05-09 01:37:06 +0800

    test(address): add test of default compressed

--- packages/ckb-sdk-address/__tests__/ckb-sdk-address.test.js
+++ packages/ckb-sdk-address/__tests__/ckb-sdk-address.test.js
@@ -13,4 +13,16 @@ describe('ckb-sdk-address', () => {
     })
     expect(address.value).toBe(fixture.address)
   })
+
+  it('generate address with default compressed which should be true', () => {
+    const fixture = {
+      privateKey: 'e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3',
+      prefix: 'ckt',
+      address: 'ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf',
+    }
+    const address = new Address(fixture.privateKey, {
+      prefix: fixture.prefix,
+    })
+    expect(address.value).toBe(fixture.address)
+  })
 })
