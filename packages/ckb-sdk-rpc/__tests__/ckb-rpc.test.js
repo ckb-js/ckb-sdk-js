248d32ab8ffb4ef7c0f11635ed05886294d2fed0
Keith <keithwhisper@gmail.com>
2019-05-15 02:53:00 +0800

    test(rpc): move rpc helpers to a single test file

--- packages/ckb-sdk-rpc/__tests__/ckb-rpc.test.js
+++ packages/ckb-sdk-rpc/__tests__/ckb-rpc.test.js
@@ -1,13 +1,10 @@
 const path = require('path')
 
 const env = path.join(__dirname, '../.env')
-
 const config = require('dotenv').config({
   path: env,
 }).parsed
-
 const CKBRPC = require('../lib').default
-const { DebugLevel } = require('../lib/enum')
 
 const rpc = new CKBRPC(config.RPC_URL)
 
@@ -137,54 +134,5 @@ describe('send transaction', () => {
 describe('ckb-rpc errors', () => {
   it('throw raw error', async () => {
     expect(() => rpc.getBlock(0)).toThrow()
-  })
-})
-
-describe('ckb-rpc settings and helpers', () => {
-  it('set node', () => {
-    const node = {
-      url: 'http://localhost:8114',
-    }
-    rpc.setNode(node)
-    expect(rpc.node).toEqual(node)
-  })
-
-  it('has 17 default rpc', () => {
-    expect(rpc.methods.length).toBe(17)
-  })
-
-  it('has initialized node url of http://localhost:8114', () => {
-    expect(rpc.methods[0].constructor.node.url).toBe('http://localhost:8114')
-  })
-
-  it('set node url to http://test.localhost:8114', () => {
-    const url = 'http://test.localhost:8114'
-    rpc.setNode({
-      url,
-    })
-    expect(rpc.methods[0].constructor.node.url).toBe(url)
-  })
-
-  it(`set debug level to ${DebugLevel.Off}`, async () => {
-    const info = jest.spyOn(global.console, 'info')
-    const group = jest.spyOn(global.console, 'group')
-    const groupEnd = jest.spyOn(global.console, 'groupEnd')
-    rpc.setDebugLevel(DebugLevel.Off)
-    await rpc.getTipBlockNumber()
-    expect(info).not.toHaveBeenCalled()
-    expect(group).not.toHaveBeenCalled()
-    expect(groupEnd).not.toHaveBeenCalled()
-  })
-
-  it(`set debug level to ${DebugLevel.On}`, async () => {
-    const info = jest.spyOn(global.console, 'info')
-    const group = jest.spyOn(global.console, 'group')
-    const groupEnd = jest.spyOn(global.console, 'groupEnd')
-    rpc.setDebugLevel(DebugLevel.On)
-    await rpc.getTipBlockNumber()
-    expect(info).toHaveBeenCalled()
-    expect(group).toHaveBeenCalled()
-    expect(groupEnd).toHaveBeenCalled()
-    rpc.setDebugLevel(DebugLevel.Off)
   })
 })
