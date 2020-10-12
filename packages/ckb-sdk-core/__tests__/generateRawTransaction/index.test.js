1a6af8975c52b120dd9e86751ae8f21b7fc16571
Keith <keithwhisper@gmail.com>
2020-10-12 16:28:03 +0800

    test(core): make getLeftCells tests independent

--- packages/ckb-sdk-core/__tests__/generateRawTransaction/index.test.js
+++ packages/ckb-sdk-core/__tests__/generateRawTransaction/index.test.js
@@ -1,4 +1,4 @@
-const { default: generateRawTransaction, getLeftCells } = require('../../lib/generateRawTransaction')
+const { default: generateRawTransaction } = require('../../lib/generateRawTransaction')
 
 const fixtures = require('./fixtures.json')
 
@@ -27,24 +27,6 @@ describe('Test generate raw transaction', () => {
       } catch (err) {
         expect(err).toEqual(new Error(exception))
       }
-    })
-  })
-
-  describe.only('getLeftCells', () => {
-    const fixtureTable = Object.entries(fixtures.getLeftCells).map(([title, { params, expected }]) => [
-      title,
-      params.map(param => ({
-        inputScripts: param.inputScripts,
-        usedCells: param.usedCells,
-        unspentCellsMap: new Map(param.unspentCellsMap),
-      })),
-      expected,
-    ])
-
-    test.each(fixtureTable)(`%s`, (_title, params, expected) => {
-      expect.assertions(1)
-
-      expect(getLeftCells(...params)).toEqual(expected)
     })
   })
 })
