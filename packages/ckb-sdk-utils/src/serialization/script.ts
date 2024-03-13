e887c7a84f61957c3dfa19e4653e302f38daaba4
Dylan Duan <duanyytop@gmail.com>
2024-03-13 13:37:34 +0800

    feat: add data2 to script hash_type

--- packages/ckb-sdk-utils/src/serialization/script.ts
+++ packages/ckb-sdk-utils/src/serialization/script.ts
@@ -13,6 +13,7 @@ export const serializeHashType = (hashType: CKBComponents.ScriptHashType) => {
   if (hashType === 'data') return '0x00'
   if (hashType === 'type') return '0x01'
   if (hashType === 'data1') return '0x02'
+  if (hashType === 'data2') return '0x04'
   throw new TypeError("Hash type must be either of 'data' or 'type'")
 }
 
